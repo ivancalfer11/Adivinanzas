119ba67be0d4c49a2b5eee54c23fea45cb839c88
ivanozil11 <ivancalfer1@gmail.com>
2019-03-08 13:07:51 +0100

    Commit 02

--- Adivinanzas.java
+++ Adivinanzas.java
@@ -5,17 +5,79 @@
  */
 package adivinanzas;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author F541U
  */
 public class Adivinanzas {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+
+    public void iniciarJuego() {
+
+        boolean adivinado = true;
+        int numeroIntentos = 0;
+        int contadorJug = 0;
+        while (adivinado == true) {
+            int puntuacion = 100;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduce tu nombre de usuario");
+            String nombreUser = sc.nextLine();
+            Jugador jugadorActual = new Jugador(nombreUser);
+
+            System.out.println("Introduce numero inicial");
+            int numeroInicial = sc.nextInt();
+            System.out.println("Introduce el numero final");
+            int numeroFinal = sc.nextInt();
+
+            Random r = new Random();
+            int numeroPensado = r.nextInt(numeroFinal - numeroInicial) + numeroInicial;
+            boolean comprobacion = false;
+            while (comprobacion == false) {
+                System.out.println("Introduce el numero que tu crees que estoy pensando");
+                int suposicion = sc.nextInt();
+
+                if (suposicion == numeroPensado) {
+                    comprobacion = true;
+                    adivinado = true;
+
+                    
+                } else {
+                    System.out.println("Error, numero incorrecto, prueba otra vez");
+                    numeroIntentos++;
+                    puntuacion = puntuacion - 10;
+                }
+
+            }
+            jugadorActual.setPuntuacion(puntuacion);
+            jugadores.add(jugadorActual);
+            ordenaCuadroDeHonor();
+            for (int i = 0; i < jugadores.size(); i++) {
+                        System.out.println(jugadores.get(i).getNombre() + "," + jugadores.get(i).getPuntuacion());
+                    }
+        }
+
+    }
+
+    private void ordenaCuadroDeHonor() {
+        ArrayList<Jugador> jugadoresOrdenados = new ArrayList<Jugador>();
+        while (!jugadores.isEmpty()) {
+            Jugador jugadorMaxPunt = jugadores.get(0);
+            for (int contador = 1; contador < jugadores.size(); contador++) {
+                if (jugadores.get(contador).getPuntuacion() > jugadorMaxPunt.getPuntuacion()) {
+                    jugadorMaxPunt = jugadores.get(contador);
+
+                }
+            }
+            jugadoresOrdenados.add(jugadorMaxPunt);
+            jugadores.remove(jugadorMaxPunt);
+
+        }
+        jugadores = jugadoresOrdenados;
     }
     
 }
---
+++ Jugador.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adivinanzas;
+
+/**
+ *
+ * @author F541U
+ */
+public class Jugador {
+    private String nombre;
+    private int puntuacion=100;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        
+    }
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    
+    
+    
+}
